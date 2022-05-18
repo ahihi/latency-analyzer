c61d0a93db58f9c9896b0f82195c5c3e48f38124
miranda kastemaa <miranda.kastemaa@aalto.fi>
2022-05-18 17:29:50 +0300

    add menubar and fix file opening bugs

--- latency_analyzer/app.py
+++ latency_analyzer/app.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from types import SimpleNamespace
 
 import librosa
@@ -31,7 +32,7 @@ class FilePlots:
     channel_analysis = self.analysis.channels[i]
     return SimpleNamespace(
       onsets = self.ax.vlines(channel_analysis.onsets, -abs_max, abs_max, label=f"onsets {channel_num}", color=color, alpha=1, linestyle="--"),
-      wave = self.ax.plot(self.time, channel_analysis.audio, label=f"channel {channel_num}", color=color, alpha=0.75)[0]
+      wave = self.ax.plot(self.time, channel_analysis.audio, label=f"wave {channel_num}", color=color, alpha=0.75)[0]
     )
 
   def update_start_trim(self, trim):
@@ -79,16 +80,26 @@ class App:
     self.canvas = FigureCanvasTkAgg(self.fig, master=self.root)
     self.canvas.draw() # TODO: refactor?
 
+    self.menubar = tk.Menu(self.root)
+    self.menu = SimpleNamespace(
+      file = tk.Menu(self.menubar, tearoff=0)
+    )
+    self.menu.file.add_command(label="Open", command=lambda *args: self.prompt_open_file(), accelerator="Ctrl+O")
+    self.menu.file.add_separator()
+    self.menu.file.add_command(label="Exit", command=lambda *args: sys.exit(0))
+    self.menubar.add_cascade(label="File", menu=self.menu.file)
+    self.root.config(menu=self.menubar)
+    self.root.bind_all("<Control-o>", lambda *args: self.prompt_open_file())
+    
     self.toolbar = NavigationToolbar2Tk(self.canvas, self.root, pack_toolbar=False)
     self.toolbar.update()
 
     self.start_trim_var = tk.IntVar()
-    self.start_trim_var.trace_add("write", self._update_start_trim)
+    self.start_trim_var.trace_add("write", lambda *args: self._update_start_trim())
     self.start_trim_widget = tk.Scale(
       root,
       from_=0, to=0, resolution=1,
       variable=self.start_trim_var, orient=tk.HORIZONTAL,
-      # length=400,
       label="start trim [samples]"
     )
 
@@ -105,7 +116,7 @@ class App:
     self.range_duration_label.pack(side=tk.TOP, anchor=tk.NW)
     self.range_duration_widget = tk.Entry(self.range_duration_frame)
     self.range_duration_widget.pack(side=tk.TOP, fill=tk.X)
-    self.range_duration_widget.bind("<Key>", lambda e: "break")
+    # self.range_duration_widget.bind("<Key>", lambda e: "break")
 
     # pack widgets
     
@@ -129,6 +140,7 @@ class App:
       },
       channels=self.options.analysis_channels
     )
+    self.ax.clear()
     self.plots = FilePlots(self.ax, self.analysis, self.options.analysis_channel_colors)
 
     self.ax.legend()
@@ -138,14 +150,22 @@ class App:
     self.ax.set_ylabel("amplitude")
 
     self.start_trim_widget.configure(to=max(0, self.analysis.num_samples-2))
+
+    self.start_trim_var.set(0)
+    self._update_start_trim(draw=False)
+    self.selected_onsets = None
+    self._update_selected_onsets(draw=False)
+
+    self.fig.canvas.draw_idle()
     
   def prompt_open_file(self):
     path = filedialog.askopenfilename(filetypes=[("WAV files", ".wav")])
-    self.open_file(path)
+    if path:
+      self.open_file(path)
 
   def _on_click(self, event):
     t = event.xdata
-    if t is None:
+    if t is None or self.analysis is None:
       return
 
     onset0 = None
@@ -161,11 +181,12 @@ class App:
     self.selected_onsets = (onset0, onset1)
     self._update_selected_onsets()
     
-  def _update_start_trim(self, *args):
+  def _update_start_trim(self, draw=True):
     self.plots.update_start_trim(self.start_trim_var.get())
-    self.fig.canvas.draw_idle()
+    if draw:
+      self.fig.canvas.draw_idle()
 
-  def _update_selected_onsets(self):
+  def _update_selected_onsets(self, draw=True):
     if self.selected_onsets is not None:
       onset0, onset1 = self.selected_onsets
       time0 = self.analysis.onsets[onset0] if onset0 is not None else 0.0
@@ -180,5 +201,6 @@ class App:
     self.plots.update_selected_onsets(time0, duration)
     self.range_duration_widget.delete(0,"end")
     self.range_duration_widget.insert(0, text)
-    self.fig.canvas.draw_idle()
+    if draw:
+      self.fig.canvas.draw_idle()
     
